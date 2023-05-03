a2f4a456076522337b26d78ad97535768f6377c7
Parth <ppmistry204@gmail.com>
2023-05-03 16:48:06 +0530

    Added about us section.

--- components/Sections/Sections.js
+++ components/Sections/Sections.js
@@ -1,33 +1,33 @@
-const Sections = () => {
-  const sections = [
-    {
-      title: 'Your First Step Towards Investment', 
-      details: 'Investing can be a great way to build wealth and secure your financial future, but it can also be daunting if you don\'t know where to start. We are here to guide you through this journey. And one of the important thing to do before you start your investing journey in stock market is to have a demat and trading account. We help you choose best trading and demat account based on your financial goals and needs. <br /><br />Please use the link below to open your free demat and trading account. Or get in touch to know more. ',
-      cta: 'Open Demat Account',
-      cardBorderClass: 'border-info',
-      imageBgClass: 'text-bg-info',
-      ctaBtnClass: 'btn-info',
-    },
-    {
-      title: 'Know More About Investing In Stocks', 
-      details: 'Investing in Indian stocks can be a lucrative opportunity for those who are willing to take calculated risks. The Indian stock market is the fifth largest in the world and offers a diverse range of investment options, from blue-chip stocks to small-cap companies. However, it\'s important to have a good understanding of the Indian economy, politics, and culture before investing. Factors such as GDP growth, inflation rates, and government policies can have a significant impact on the stock market. In addition, investors should conduct thorough research on individual companies and sectors to identify potential opportunities and risks. With the right strategy and approach, investing in Indian stocks can provide long-term growth and diversification to an investment portfolio.',
-      cta: 'Start Investing/ Get In Touch',
-      cardBorderClass: 'border-secondary',
-      imageBgClass: 'text-bg-secondary',
-      ctaBtnClass: 'btn-secondary',
-    },
-    {
-      title: 'Want to Start Trading with zero knowledge & free of cost?', 
-      details: 'Algorithmic trading, also known as algo trading, is becoming increasingly popular in the Indian stock market. It involves using computer programs and algorithms to automate trades based on pre-set rules and conditions. Algo trading can provide several benefits such as faster and more accurate trade execution, reduced emotional bias, and the ability to analyze large amounts of data quickly. However, it\'s important to note that algo trading also carries certain risks, such as technical glitches or market volatility. It\'s crucial for traders to have a thorough understanding of the algorithms being used and to continuously monitor and adjust their strategies. With the right approach and risk management, algo trading can provide a competitive edge and potentially higher returns in the Indian stock market.',
-      cta: 'Know More',
-      cardBorderClass: 'border-success',
-      imageBgClass: 'text-bg-success',
-      ctaBtnClass: 'btn-success',
-    },
-  ]
+const sections = [
+  {
+    title: 'Your First Step Towards Investment', 
+    details: 'Investing can be a great way to build wealth and secure your financial future, but it can also be daunting if you don\'t know where to start. We are here to guide you through this journey. And one of the important thing to do before you start your investing journey in stock market is to have a demat and trading account. We help you choose best trading and demat account based on your financial goals and needs. <br /><br />Please use the link below to open your free demat and trading account. Or get in touch to know more. ',
+    cta: 'Open Demat Account',
+    cardBorderClass: 'border-info',
+    imageBgClass: 'text-bg-info',
+    ctaBtnClass: 'btn-info',
+  },
+  {
+    title: 'Know More About Investing In Stocks', 
+    details: 'Investing in Indian stocks can be a lucrative opportunity for those who are willing to take calculated risks. The Indian stock market is the fifth largest in the world and offers a diverse range of investment options, from blue-chip stocks to small-cap companies. However, it\'s important to have a good understanding of the Indian economy, politics, and culture before investing. Factors such as GDP growth, inflation rates, and government policies can have a significant impact on the stock market. In addition, investors should conduct thorough research on individual companies and sectors to identify potential opportunities and risks. With the right strategy and approach, investing in Indian stocks can provide long-term growth and diversification to an investment portfolio.',
+    cta: 'Start Investing/ Get In Touch',
+    cardBorderClass: 'border-secondary',
+    imageBgClass: 'text-bg-secondary',
+    ctaBtnClass: 'btn-secondary',
+  },
+  {
+    title: 'Want to Start Trading with zero knowledge & free of cost?', 
+    details: 'Algorithmic trading, also known as algo trading, is becoming increasingly popular in the Indian stock market. It involves using computer programs and algorithms to automate trades based on pre-set rules and conditions. Algo trading can provide several benefits such as faster and more accurate trade execution, reduced emotional bias, and the ability to analyze large amounts of data quickly. However, it\'s important to note that algo trading also carries certain risks, such as technical glitches or market volatility. It\'s crucial for traders to have a thorough understanding of the algorithms being used and to continuously monitor and adjust their strategies. With the right approach and risk management, algo trading can provide a competitive edge and potentially higher returns in the Indian stock market.',
+    cta: 'Know More',
+    cardBorderClass: 'border-success',
+    imageBgClass: 'text-bg-success',
+    ctaBtnClass: 'btn-success',
+  },
+];
+const Sections = ({ sectionData = sections}) => {
   return <div className="sections">
     <div className="row">
-      {sections.map((s, index) => {
+      {sectionData.map((s, index) => {
         if (index % 2 === 0) {
           return <div className="col-sm-12" key={index}>
               <div className={`card ${s.cardBorderClass}`}>
--- pages/about-us.js
+++ pages/about-us.js
@@ -1,9 +1,31 @@
 import NavBar from "../components/NavBar/NavBar"
+import Sections from "../components/Sections/Sections";
 
 const AboutUs = props => {
+    const FIRM_NAME = 'Free Demat A/C Services'
+    const sections = [
+        {
+            title: 'ABOUT US', 
+            details: `
+            ${FIRM_NAME} is a comprehensive financial consulting firm that offers various finanacial services to help clients manage their investments more efficiently. With ${FIRM_NAME}'s financial services, clients can:<br />
+            <br/>- <b>Open a Demat Account</b>: ${FIRM_NAME} offers a hassle-free process to open a demat account, which is necessary to hold and trade securities in India.
+            <br />- <b>Lost Share Recovery</b>: ${FIRM_NAME} assists clients in recovering lost or unclaimed shares through the <b>Investor Education and Protection Fund (IEPF)</b>.
+            <br /><br />In addition to its demat and trading services, ${FIRM_NAME} also offers a range of services to help clients achieve their financial goals. With a team of experienced professionals, ${FIRM_NAME} provides personalized and tailored solutions to suit individual needs.<br /><br />Some of the key services offered by ${FIRM_NAME} include:
+            <br />- <b>Financial Planning</b>: ${FIRM_NAME} helps clients develop a holistic financial plan to achieve long-term financial stability and security.
+            <br />- <b>Investment Management</b>: ${FIRM_NAME} provides expert investment advice and management services to help clients maximize returns and minimize risks.
+            <br />- <b>Tax Planning</b>: ${FIRM_NAME} offers customized tax planning solutions to help clients optimize their tax liabilities and take advantage of available tax-saving opportunities.
+            <br /><br />
+            Whether you are an individual, family, or business, ${FIRM_NAME} can provide the guidance and support you need to achieve your financial objectives.`,
+            cta: 'Get In Touch',
+            cardBorderClass: 'border-success',
+            imageBgClass: 'text-bg-success',
+            ctaBtnClass: 'btn-success',
+          },
+    ]
     return <div className="main-container">
         <NavBar activeLink="aboutUs" />
-        <h2>About Us</h2>
+        {/* <h2>About Us</h2> */}
+        <Sections sectionData={sections}/>
     </div>
 }
 
